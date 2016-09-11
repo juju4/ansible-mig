cb48c856cf82897c8a541f6fddaaae4878ac62ab
juju4 <juju4@users.noreply.github.com>
2016-09-11 16:20:20 -0400

    missing golang role dependency

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,6 +9,7 @@ rolesdir=$(dirname $0)/..
 [ ! -d $rolesdir/adduser ] && git clone https://github.com/juju4/ansible-adduser $rolesdir/adduser
 [ ! -d $rolesdir/gpgkey_generate ] && git clone https://github.com/juju4/ansible-gpgkey_generate $rolesdir/gpgkey_generate
 [ ! -d $rolesdir/playbook-rabbitmq ] && git clone https://github.com/juju4/ansible-playbook-rabbitmq $rolesdir/Mayeu.RabbitMQ
+[ ! -d $rolesdir/golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/golang
 [ ! -d $rolesdir/mig-api ] && git clone https://github.com/juju4/ansible-mig-api $rolesdir/mig-api
 [ ! -d $rolesdir/mig-scheduler ] && git clone https://github.com/juju4/ansible-mig-scheduler $rolesdir/mig-scheduler
 [ ! -d $rolesdir/mig-postgres ] && git clone https://github.com/juju4/ansible-mig-postgres $rolesdir/mig-postgres
